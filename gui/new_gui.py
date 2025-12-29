621cc8da0cca3340ab8931a1333fb5491c7af985
Albert <atsukhv@gmail.com>
2025-12-29 21:37:30 +0300

    Victory

--- gui/new_gui.py
+++ gui/new_gui.py
@@ -15,52 +15,68 @@ FONT_TEXT = ("Arial", 14)
 
 #----------- Окно -----------
 app = ctk.CTk()
-app.title("Manga on Kindle tool")
-app.geometry("890x500")
-app.resizable(False, True) #TODO заменить на false
+app.title("Manga tool")
+app.geometry("890x570") #570
+app.resizable(False, False)
 app.configure(fg_color=BG)
+
+app.grid_rowconfigure(0, weight=0)
+app.grid_rowconfigure(1, weight=0)
+app.grid_columnconfigure(0, weight=1)
+
 
 #----------- Контейнер -----------
 container = ctk.CTkFrame(app, fg_color="transparent")
-container.grid(row=0, column=0, sticky="nsew", padx=30, pady=30)
+container.grid(row=0, column=0, sticky="ew", padx=25, pady=25)
 
-app.grid_rowconfigure(0, weight=1)
-app.grid_columnconfigure(0, weight=1)
-
-container.grid(row=0, column=0, sticky="nsew", padx=30, pady=30)
-container.grid_columnconfigure(0, weight=1)
-container.grid_columnconfigure(1, weight=1)
-
-
-
+container.grid_columnconfigure(0, minsize=420, weight=0)
+container.grid_columnconfigure(1, minsize=420, weight=0)
+container.grid_rowconfigure(0, weight=0)
+container.grid_rowconfigure(1, weight=0)
+container.grid_rowconfigure(2, weight=0)
 
 
 #----------- Фреймы -----------
 header_frame = ctk.CTkFrame(container,fg_color="transparent")
-header_frame.grid(row=0,column=0,sticky="ew",pady=(0, 24))
+header_frame.grid(row=0, column=0, columnspan=2, sticky="w", pady=(0, 20))
 
-search_frame = ctk.CTkFrame(container, fg_color="transparent")
-search_frame.grid(row=1, column=0, columnspan=2, sticky="ew", pady=(0, 24))
+search_frame = ctk.CTkFrame(container, fg_color="transparent") # TODO изменить длинну поисковой строки что бы не выходило за пределы других фрэймов
+search_frame.grid(row=1, column=0, columnspan=2, sticky="ew", pady=(0, 20))
 
 folder_frame = ctk.CTkFrame(container, fg_color="transparent")
-folder_frame.grid(row=2, column=0, sticky="ew", padx=(0, 20))
-folder_frame.grid_columnconfigure(0, weight=0)
-folder_frame.grid_columnconfigure(1, weight=1)
+folder_frame.grid(row=2, column=0, sticky="w", padx=(0, 20))
 
+name_frame = ctk.CTkFrame(container, fg_color="transparent")
+name_frame.grid(row=2, column=1, sticky="w", pady=(0, 0))
+
+select_chapters_frame = ctk.CTkFrame(container, fg_color="transparent")
+select_chapters_frame.grid(row=3, column=0, sticky="w", pady=(0, 20))
+
+downloads_frame = ctk.CTkFrame(container, fg_color="transparent")
+downloads_frame.grid(row=3, column=1, sticky="w", pady=(0, 20))
+
+info_frame = ctk.CTkFrame(container, fg_color="transparent")
+info_frame.grid(row=4, column=0, columnspan=2, sticky="w", pady=(0, 0))
 
 config_frame = ctk.CTkFrame(container, fg_color="transparent")
-config_frame.grid(row=2, column=1, sticky="ew", padx=(0, 0))
+config_frame.grid(row=5, column=0, sticky="w", pady=(0, 0))
 
-
-
-
+sorting_frame = ctk.CTkFrame(container, fg_color="transparent")
+sorting_frame.grid(row=5, column=1, sticky="w", pady=(0, 20))
 
 progress_frame = ctk.CTkFrame(app, fg_color="transparent")
-progress_frame.grid(row=1, column=0, sticky="ew")
+progress_frame.grid(row=2, column=0, sticky="ew", pady=(0, 0))
+
+app.grid_rowconfigure(0, weight=0)     # container сверху
+app.grid_rowconfigure(1, weight=1)     # пустое место для растяжки
+app.grid_rowconfigure(2, weight=0)     # progress_frame снизу
+app.grid_columnconfigure(0, weight=1)
 
 #----------- Заголовок -----------
-title = ctk.CTkLabel(header_frame, text="Manga on Kindle tool", font=FONT_TITLE, text_color=ACCENT)
-title.grid(row=0, column=0, sticky="w")
+title = ctk.CTkLabel(header_frame, text="Manga tool", font=FONT_TITLE, text_color=ACCENT)
+title.grid(row=0, column=0, columnspan=2, sticky="w")
+
+
 
 #----------- Поле для ввода ссылки -----------
 # Поле для ввода ссылки
@@ -90,17 +106,19 @@ url_discription.grid(row=1, column=0, columnspan=2, sticky="w", pady=(4,0))
 search_frame.grid_columnconfigure(0, weight=1)  # entry растягивается
 search_frame.grid_columnconfigure(1, weight=0)  # кнопка фиксированная
 
-#----------- Выбор папки (grid) -----------
+
+
+#----------- Выбор папки -----------
 folder_icon = ctk.CTkImage(Image.open("icons/folder_icon.png"), size=(20, 21))
 
 # Верхняя подпись
 folder_discription = ctk.CTkLabel(folder_frame, text="Выберите папку для загрузки файлов",
                                   font=FONT_TEXT, text_color=TEXT_COLOR)
-folder_discription.grid(row=0, column=0, columnspan=2, sticky="w", pady=(0,4))
+folder_discription.grid(row=0, column=0, columnspan=2, sticky="w", pady=(0,0))
 
 # Кнопка слева и поле пути справа
 folder_btn = ctk.CTkButton(folder_frame, image=folder_icon, text="",
-                           width=20, height=21,
+                           width=30, height=30,
                            fg_color=ACCENT, hover_color="#FFA733",
                            corner_radius=0, border_width=0,
                            anchor="center",
@@ -111,62 +129,15 @@ selected_folder_label = ctk.CTkLabel(folder_frame, text="   Папка не вы
                                      width=366, height=30,
                                      fg_color=SECOND_BG, text_color=TEXT_COLOR,
                                      corner_radius=0, anchor="w")
-selected_folder_label.grid(row=1, column=1, sticky="w", padx=(5,0))
+selected_folder_label.grid(row=1, column=1, sticky="w")
 
 # Растяжка колонок
 folder_frame.grid_columnconfigure(0, weight=0)
-folder_frame.grid_columnconfigure(1, weight=1)
-
-
-#----------- Выбор конфига -----------
-open_folder_icon = ctk.CTkImage(Image.open("icons/open_folder_icon.png"), size=(20, 21))
-
-# Верхние подписи
-open_folder_discription = ctk.CTkLabel(config_frame, text="Открыть конфиг",
-                                       font=FONT_TEXT, text_color=TEXT_COLOR)
-folder_discription.grid(row=0, column=0, columnspan=2, sticky="w", pady=(0,4))
-
-combo_label = ctk.CTkLabel(config_frame, text="Выбрать конфиг",
-                           font=FONT_TEXT, text_color=TEXT_COLOR)
-combo_label.grid(row=0, column=1, sticky="w")
-
-# Кнопка слева и ComboBox справа в одной строке
-button_combo_frame = ctk.CTkFrame(config_frame, fg_color="transparent")  # вложенный фрейм для "склеивания"
-button_combo_frame.grid(row=1, column=0, columnspan=2, sticky="w")
-
-open_folder_btn = ctk.CTkButton(button_combo_frame,
-                                image=open_folder_icon,
-                                text="",
-                                width=20,
-                                height=21,
-                                fg_color=ACCENT,
-                                hover_color="#FFA733",
-                                corner_radius=0,
-                                border_width=0,
-                                anchor="center",
-                                command=lambda: choose_folder(selected_folder_label))
-open_folder_btn.pack(side="left")  # слева
-
-combo_box = ctk.CTkComboBox(button_combo_frame,
-                            values=get_delete_configs(),
-                            width=366,
-                            height=30,
-                            corner_radius=0,
-                            fg_color=SECOND_BG,
-                            button_color=ACCENT,
-                            button_hover_color="#FFA733",
-                            dropdown_fg_color=SECOND_BG,
-                            text_color=TEXT_COLOR,
-                            border_width=0)
-combo_box.pack(side="left", padx=(0,0))  # прямо к кнопке, без промежутка
-
-# Колонки config_frame
-config_frame.grid_columnconfigure(0, weight=0)
-config_frame.grid_columnconfigure(1, weight=1)
+folder_frame.grid_columnconfigure(1, weight=0)
 
 #----------- Инструкция -----------
 info_text = ctk.CTkLabel(
-    container,
+    info_frame,
     text="После загрузки посмотрите есть ли какие либо паттерны мусорных файлов, которые вы хотели бы удалить, "
          "например обложки или доп. рисуноки. Выпишите нзавания этих файлов в конфиг и на следующем этапе они будут "
          "удалены из всех томов и глав.",
@@ -175,22 +146,17 @@ info_text = ctk.CTkLabel(
     wraplength=827,
     justify="left"
 )
-info_text.place(x=0, y=250)
+info_text.grid(row=0, column=0, sticky="w")
 
 
 #----------- Название -----------
-name_text = ctk.CTkLabel(
-    container,
-    text="Задайте название для манги",
-    font=FONT_TEXT,
-    text_color=TEXT_COLOR
-)
-name_text.place(x=0, y=324)
+name_text = ctk.CTkLabel(name_frame, text="Задайте название для манги", font=FONT_TEXT,text_color=TEXT_COLOR)
+name_text.grid(row=0, column=0, sticky="w")
 
 name_entry = ctk.CTkEntry(
-    container,
+    name_frame,
     width=396,
-    height=40,
+    height=30,
     corner_radius=0,
     fg_color=SECOND_BG,
     border_width=0,
@@ -199,14 +165,14 @@ name_entry = ctk.CTkEntry(
     placeholder_text="Например: Berserk",
     justify="left"
 )
-name_entry.place(x=0, y=354)
+name_entry.grid(row=1, column=0, sticky="w")
 
 #----------- Сортировка -----------
 start_sorting = ctk.CTkButton(
-    container,
+    sorting_frame,
     text="Запустить сортировку файлов",
     width=396,
-    height=40,
+    height=30,
     fg_color=ACCENT,
     hover_color="#FFA733",
     corner_radius=0,
@@ -214,9 +180,150 @@ start_sorting = ctk.CTkButton(
     anchor="center",
     command=lambda: choose_folder(selected_folder_label)
 )
-start_sorting.place(x=434, y=354)
+start_sorting.grid(row=0, column=1, sticky="w", pady=(50, 0))
 
-#----------- Прогресс-бар -----------
+
+# ---------- Надпись ----------
+select_label = ctk.CTkLabel(
+    select_chapters_frame,
+    text="Выберите главы:",
+    font=FONT_TEXT,
+    text_color=TEXT_COLOR
+)
+select_label.grid(row=0, column=0, pady=(20, 0), columnspan=4, sticky="w")
+
+# ---------- Выбор диапазона глав ----------
+from_label = ctk.CTkLabel(
+    select_chapters_frame,
+    text="От:",
+    font=FONT_TEXT,
+    text_color=TEXT_COLOR
+)
+from_label.grid(row=1, column=0,)
+
+from_entry = ctk.CTkEntry(
+    select_chapters_frame,
+    width=50,
+    height=30,
+    corner_radius=0,
+    fg_color=SECOND_BG,
+    border_width=0,
+    text_color=TEXT_COLOR,
+    font=FONT_TEXT,
+    placeholder_text="1"
+)
+from_entry.grid(row=1, column=1, padx=(10, 0))
+
+to_label = ctk.CTkLabel(
+    select_chapters_frame,
+    text="До:",
+    font=FONT_TEXT,
+    text_color=TEXT_COLOR
+)
+to_label.grid(row=1, column=2, padx=(20, 0))
+
+to_entry = ctk.CTkEntry(
+    select_chapters_frame,
+    width=50,
+    height=30,
+    corner_radius=0,
+    fg_color=SECOND_BG,
+    border_width=0,
+    text_color=TEXT_COLOR,
+    font=FONT_TEXT,
+    placeholder_text="100"
+)
+to_entry.grid(row=1, column=3, padx=(10, 0))
+
+skip_label = ctk.CTkLabel(
+    select_chapters_frame,
+    text="Пропустить:",
+    font=FONT_TEXT,
+    text_color=TEXT_COLOR
+)
+skip_label.grid(row=1, column=4, padx=(20, 0))
+
+skip_entry = ctk.CTkEntry(
+    select_chapters_frame,
+    width=100,
+    height=30,
+    corner_radius=0,
+    fg_color=SECOND_BG,
+    border_width=0,
+    text_color=TEXT_COLOR,
+    font=FONT_TEXT,
+    placeholder_text="3,7,12"
+)
+skip_entry.grid(row=1, column=5, padx=(10, 0))
+
+# ---------- Кнопка Скачать ----------
+download_btn = ctk.CTkButton(
+    downloads_frame,
+    text="Скачать",
+    width=396,
+    height=30,
+    fg_color=ACCENT,
+    hover_color="#FFA733",
+    corner_radius=0,
+    border_width=0,
+    font=FONT_TEXT,
+    anchor="center",
+)
+download_btn.grid(row=0, column=0, pady=(48, 0))
+
+
+
+
+
+#----------- Выбор конфига 2 -----------
+open_folder_icon = ctk.CTkImage(Image.open("icons/open_folder_icon.png"), size=(20, 21))
+
+# ----- Верхние подписи -----
+label_open = ctk.CTkLabel(
+    config_frame,
+    text="Открыть конфиг                                             Выбрать конфиг",
+    font=FONT_TEXT,
+    text_color=TEXT_COLOR
+)
+label_open.grid(row=0, column=0, sticky="w", columnspan=6)
+
+
+# ----- Кнопка открытия и ComboBox -----
+open_btn = ctk.CTkButton(
+    config_frame,
+    image=open_folder_icon,
+    text="",
+    width=30,
+    height=30,
+    fg_color=ACCENT,
+    hover_color="#FFA733",
+    corner_radius=0,
+    border_width=0,
+    command=lambda: choose_folder(selected_folder_label)
+)
+open_btn.grid(row=1, column=0, sticky="w")
+
+combo_box = ctk.CTkComboBox(
+    config_frame,
+    values=get_delete_configs(),
+    width=366,
+    height=30,
+    corner_radius=0,
+    fg_color=SECOND_BG,
+    button_color=ACCENT,
+    button_hover_color="#FFA733",
+    dropdown_fg_color=SECOND_BG,
+    text_color=TEXT_COLOR,
+    border_width=0
+)
+combo_box.grid(row=1, column=1, sticky="w",)
+
+
+
+
+
+
+# ----------- Прогресс-бар -----------
 progress = ctk.CTkProgressBar(
     progress_frame,
     height=15,
@@ -224,7 +331,7 @@ progress = ctk.CTkProgressBar(
     fg_color=SECOND_BG,
     progress_color=ACCENT
 )
-progress.pack(side="bottom", fill="x")
+progress.pack(side="bottom", fill="x", pady=(0, 0))
 progress.set(0)
 
 progress_text = ctk.CTkLabel(
@@ -235,99 +342,7 @@ progress_text = ctk.CTkLabel(
     anchor="w",
     padx=5
 )
-progress_text.pack(side="bottom", fill="x")
-
-
-# ---------- Надпись ----------
-select_label = ctk.CTkLabel(
-    container,
-    text="Выберите главы:",
-    font=FONT_TEXT,
-    text_color=TEXT_COLOR
-)
-select_label.place(x=0, y=370)  # чуть выше строки с полями
-
-# ---------- Выбор диапазона глав ----------
-from_label = ctk.CTkLabel(
-    container,
-    text="От:",
-    font=FONT_TEXT,
-    text_color=TEXT_COLOR
-)
-from_label.place(x=0, y=400)
-
-from_entry = ctk.CTkEntry(
-    container,
-    width=50,
-    height=30,
-    corner_radius=0,
-    fg_color=SECOND_BG,
-    border_width=0,
-    text_color=TEXT_COLOR,
-    font=FONT_TEXT,
-    placeholder_text="1"
-)
-from_entry.place(x=30, y=400)
-
-to_label = ctk.CTkLabel(
-    container,
-    text="До:",
-    font=FONT_TEXT,
-    text_color=TEXT_COLOR
-)
-to_label.place(x=90, y=400)
-
-to_entry = ctk.CTkEntry(
-    container,
-    width=50,
-    height=30,
-    corner_radius=0,
-    fg_color=SECOND_BG,
-    border_width=0,
-    text_color=TEXT_COLOR,
-    font=FONT_TEXT,
-    placeholder_text="10"
-)
-to_entry.place(x=120, y=400)
-
-skip_label = ctk.CTkLabel(
-    container,
-    text="Пропустить:",
-    font=FONT_TEXT,
-    text_color=TEXT_COLOR
-)
-skip_label.place(x=180, y=400)
-
-skip_entry = ctk.CTkEntry(
-    container,
-    width=100,
-    height=30,
-    corner_radius=0,
-    fg_color=SECOND_BG,
-    border_width=0,
-    text_color=TEXT_COLOR,
-    font=FONT_TEXT,
-    placeholder_text="3,7,12"
-)
-skip_entry.place(x=260, y=400)
-
-# ---------- Кнопка Скачать ----------
-download_btn = ctk.CTkButton(
-    container,
-    text="Скачать",
-    width=120,
-    height=30,
-    fg_color=ACCENT,
-    hover_color="#FFA733",
-    corner_radius=0,
-    border_width=0,
-    font=FONT_TEXT,
-    anchor="center",
-)
-download_btn.place(x=380, y=400)
-
-
-
+progress_text.pack(side="top", fill="x", pady=(0, 0))      # текст сверху
 
 if __name__ == "__main__":
     app.mainloop()
